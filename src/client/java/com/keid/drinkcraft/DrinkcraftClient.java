0feac8373a53ecfcac0d9f7547caa7e7b93bd8a9
very secret <lavaempteybukkit@gmail.com>
2024-12-05 23:30:50 +0100

    Initial commit

---
+++ src/client/java/com/keid/drinkcraft/DrinkcraftClient.java
@@ -0,0 +1,10 @@
+package com.keid.drinkcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrinkcraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
